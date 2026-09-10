fe8831899e3d6f398f16a82a16c535d7931a00d3
D4rku <adrien.roj01@gmail.com>
2026-09-10 12:30:33 +0200

    Ajoute la possibiliter de heal un autre joueur dans la partie

--- src/main/java/LeRoj/mon_Premier_Plugin/HealCommand.java
+++ src/main/java/LeRoj/mon_Premier_Plugin/HealCommand.java
@@ -5,15 +5,32 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static org.bukkit.Bukkit.getPlayer;
+
 public class HealCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            player.setHealth(player.getMaxHealth());
-            player.sendMessage("Vos HP on été restaurés !");
-            return true;
+            if(args.length == 0) {
+                player.setHealth(player.getMaxHealth());
+                player.sendMessage("Vos HP on été restaurés !");
+                return true;
+            }
+            else{
+                Player cible = getPlayer(args[0]);
+                if(cible != null){
+                    cible.setHealth(cible.getMaxHealth());
+                    cible.sendMessage("Vos HP on été restaurés !");
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
         }
         else {
             return false;
